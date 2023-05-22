69e28f08593cf2bae5af630ee77be68395292692
Mahanaim <castromahanaim@gmail.com>
2023-05-22 15:29:25 -0600

    cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
 const cipher = {
-  // ...
+  encode: function (desplazamiento,texto) {
+    if (!texto.length || !desplazamiento) {
+      throw new TypeError()
+    }
+    texto = texto.toUpperCase()
+    let resultado = "";
+    const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    if (texto) {
+      for (let i = 0; i < texto.length; i++) {
+        const posicion = (abecedario.indexOf(texto[i]) + Number(desplazamiento)) % 26
+
+        resultado += abecedario[posicion];
+
+      }
+      return resultado;
+    }
+  },
+  decode: function (desplazamiento,texto) {
+    if (!texto.length || !desplazamiento) {
+      throw new TypeError()
+    }
+    
+    let resultado = "";
+    const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+    if (texto) {
+      for (let i = 0; i < texto.length; i++) {
+        let posicion = ((abecedario.indexOf(texto[i]) - Number(desplazamiento)) % 26);
+        if (posicion < 0) {
+          posicion = 26 + posicion
+
+        }
+        resultado += abecedario[posicion];
+      }
+      return resultado;
+    }
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+window.addEventListener("load", inicio, true);
+//funcion botones
+function inicio() {
+  const cifrar = document.getElementById("cifrar");
+
+  cifrar.addEventListener("click", function () {
+    const texto = document.getElementById("crear").value;
+    const desplazamiento = document.getElementById("desplazamiento").value;
+    document.getElementById("clave").value = cipher.cifrar(desplazamiento, texto);
+  }, true);
+  const descifrar = document.getElementById("descifrar");
+  descifrar.addEventListener("click", function () {
+    const texto = document.getElementById("crear").value;
+    const desplazamiento = document.getElementById("desplazamiento").value;
+    document.getElementById("clave").value = cipher.descifrar(desplazamiento, texto);
+  }, true);
+}
